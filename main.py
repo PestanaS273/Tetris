25d954bbc67de5f703bd2f4bf8da88d7cbe0257a
PestanaS273 <118370117+PestanaS273@users.noreply.github.com>
2023-06-14 09:47:43 +0200

    First

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+import sys
+
+class Game:
+    def __int__(self):
+        pygame.init()
+        pygame.mixer.init()
+
+
+# if __name__ == '__main__':
+#     game = Game()
+#     game.run()
