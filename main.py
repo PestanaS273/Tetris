ce9e0e27264551b2d80b0c2f1da1d8e9b1f6c1ca
PestanaS273 <118370117+PestanaS273@users.noreply.github.com>
2023-06-26 17:34:50 +0200

    add functional interface

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class App:
 
         self.paused = False
         self.over = False
+        self.start = False
         
 
 
@@ -51,6 +52,63 @@ class App:
         self.tetris.draw()
         self.text.draw()
         pg.display.flip()
+        
+
+    def game_start(self):
+        start = True
+        
+        pg.mixer.music.stop()
+        game_start_screen_fade = pg.Surface(WINDOW_RESOLUTION)
+        game_start_screen_fade.fill((105,13,210))
+        self.screen.blit(game_start_screen_fade, (0, 0))
+
+
+
+        self.text.font.render_to(self.screen, (FIELD_WIDTH* 5, FIELD_HEIGHT* 5),
+                            text="T", fgcolor='red',
+                            size=TILE_SIZE * 3)
+        self.text.font.render_to(self.screen, (FIELD_WIDTH* 15, FIELD_HEIGHT* 5),
+                            text="E", fgcolor='orange',
+                            size=TILE_SIZE * 3)
+        self.text.font.render_to(self.screen, (FIELD_WIDTH* 25, FIELD_HEIGHT* 5),
+                            text="T", fgcolor='yellow',
+                            size=TILE_SIZE * 3)
+        self.text.font.render_to(self.screen, (FIELD_WIDTH* 35, FIELD_HEIGHT* 5),
+                            text="R", fgcolor='green',
+                            size=TILE_SIZE * 3)
+        self.text.font.render_to(self.screen, (FIELD_WIDTH* 45, FIELD_HEIGHT* 5),
+                            text="I", fgcolor='cyan',
+                            size=TILE_SIZE * 3)
+        self.text.font.render_to(self.screen, (FIELD_WIDTH* 55, FIELD_HEIGHT* 5),
+                            text="S", fgcolor='violet',
+                            size=TILE_SIZE * 3)
+        
+        self.text.font.render_to(self.screen, (FIELD_WIDTH* 11, FIELD_HEIGHT* 20),
+                            text="One Player : Press 1", fgcolor='white',
+                            size=TILE_SIZE * 0.90, bgcolor=(25, 7, 118))
+        
+        self.text.font.render_to(self.screen, (FIELD_WIDTH* 10, FIELD_HEIGHT* 25),
+                            text="Two Players : Press 2", fgcolor='white',
+                            size=TILE_SIZE * 0.90, bgcolor=(25, 7, 118))
+        
+        
+        
+        pg.display.update()
+        
+        while start:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_1:
+                        start = False
+                        self.start = True
+                        pg.mixer.music.play(-1)
+                        pg.mixer.music.set_volume(0.2)
+                    
+            self.clock.tick(5)
+
 
 
     def game_paused(self):
@@ -145,12 +203,26 @@ class App:
                 
         
 
+#def run original 
+    # def run(self):
+    #     while True:
+    #         self.check_events()
+            
+    #         if not self.over:
+    #             self.update()
+    #             self.draw()
+                
+    #         else: 
+    #             self.game_over()
 
     def run(self):
         while True:
             self.check_events()
             
-            if not self.over:
+            if not self.start:
+                self.game_start()
+            
+            elif not self.over:
                 self.update()
                 self.draw()
                 
